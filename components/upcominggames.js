2642195f49714ef7d44558fb842ceb550e12ab48
Andrew Pierce <andrunix@gmail.com>
2021-11-04 17:58:36 -0400

    Making background image stretch and footer stick to bottom correctly.

--- components/upcominggames.js
+++ components/upcominggames.js
@@ -10,20 +10,20 @@ export default function UpcomingGames({schedule}) {
       <h2 className="text-2xl font-bold border-b-2 text-blue-500">Upcoming games</h2>
     </div>
     <div className="bg-white px-4">
-      <div className="px-4 py-6 flex-row rounded-md drop-shadow-xl">
+      <div className="px-4 py-2 flex-row rounded-md drop-shadow-xl">
         {schedule.filter(game => new Date(game.start_date).getTime() >= now).map(game => (
           <div key={game.id} className="m-2 p-2 hover:bg-blue-100 border rounded-sm">
-            <div className="inline-block flex-none w-1/3 p-2">
+            <div className="inline-block flex-none w-1/3">
               <Link href={`/teams/${normalizeName(game.home_team)}`}>
                 <a className="text-blue-500 hover:text-blue-400">{game.home_team}</a>
               </Link>
             </div>
-            <div className="inline-block flex-none w-1/3 p-2">
+            <div className="inline-block flex-none w-1/3">
               <Link href={`/teams/${normalizeName(game.away_team)}`}>
                 <a className="text-blue-500 hover:text-blue-400">{game.away_team}</a>
               </Link>
             </div>
-            <div className="inline-block flex-none w-1/3 p-2">
+            <div className="inline-block flex-none w-1/3">
               {formatDate(game.start_date, false, true)}
             </div>
           </div>
