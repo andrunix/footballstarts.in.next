86d0deaee8331ab6e69f869393f7071aa223dc4e
Andrew Pierce <andrunix@gmail.com>
2021-11-04 20:29:19 -0400

    Updating layout. Got rid of table on the schedule page.

--- components/schedule.js
+++ components/schedule.js
@@ -16,40 +16,29 @@ export default function Schedule({schedule}) {
   const nextGame = findNextGame(schedule);
   
   return (
-    <div className="bg-white pb-2 rounded-b-md sm:px-4">
+    <>
       <div className="bg-white px-4 py-2">
         <h2 className="text-2xl font-bold border-b-2 text-blue-500">Schedule</h2>
       </div>
-
-      <div className="bg-white px-1 md:px-4">
-        <table className="table-fixed text-xl">
-          <thead>
-            <tr>
-              <th className="w-1/3 text-left">Opponent</th>
-              <th>Date</th>
-              <th>&nbsp;</th>
-              <th>Score</th>
-              <th>&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            {schedule.map((game) => (
-              <tr key={game.id} className={game.id === nextGame ? "bg-green-100 border border-green-400" : "border-t"}>
-                <td>
-                  <Link href={`/teams/${game.normalizedOpponent}`}>
-                    <a className="text-blue-600 hover:text-blue-900">{(game.homeGame) ? game.opponent : '@' + game.opponent}</a>
-                  </Link>
-                </td>
-                <td className="text-center">{formatDate(game.start_date)}</td>
-                <td>{formatTime(game.start_date)}</td>
-                <td className="text-center">{(game.home_points) ? `${game.home_points}-${game.away_points}` : ''}</td>
-                <td className={game.winLoss === 'W' ? "win" : "loss"}>{game.winLoss}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="bg-white px-2 md:px-4 py-2">
+        <div className="py-2 flex-row rounded-md drop-shadow-xl">
+          {schedule.map((game) => (
+            <div key={game.id} className={["m-1 p-2 hover:bg-blue-100 border rounded-sm", game.id === nextGame ? "bg-green-100 border border-green-400" : "border-t"].join(" ")}>
+              
+              <div className="inline-block flex-none w-1/4">
+                <Link href={`/teams/${game.normalizedOpponent}`}>
+                  <a className="text-blue-600 hover:text-blue-900">{(game.homeGame) ? game.opponent : '@' + game.opponent}</a>
+                </Link>
+              </div>
+              <div className="inline-block flex-none w-1/6 text-center">{formatDate(game.start_date)}</div>
+              <div className="inline-block flex-none w-1/6 text-left">{formatTime(game.start_date)}</div>
+              <div className="inline-block flex-none w-1/6 text-center">{(game.home_points) ? `${game.home_points}-${game.away_points}` : ''}</div>
+              <div className={["inline-block flex-none w-1/8 text-left", game.winLoss === 'W' ? "win" : "loss"].join(" ")}>{game.winLoss}</div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- components/upcominggames.js
+++ components/upcominggames.js
@@ -6,13 +6,13 @@ export default function UpcomingGames({schedule}) {
   const now = new Date().getTime();
   return (
     <>
-    <div className="bg-white px-4 py-2">
+    <div className="bg-white px-2 py-2">
       <h2 className="text-2xl font-bold border-b-2 text-blue-500">Upcoming games</h2>
     </div>
-    <div className="bg-white px-4">
-      <div className="px-4 py-2 flex-row rounded-md drop-shadow-xl">
+    <div className="bg-white px-2">
+      <div className="py-2 flex-row rounded-md drop-shadow-xl">
         {schedule.filter(game => new Date(game.start_date).getTime() >= now).map(game => (
-          <div key={game.id} className="m-2 p-2 hover:bg-blue-100 border rounded-sm">
+          <div key={game.id} className="m-1 p-2 hover:bg-blue-100 border rounded-sm">
             <div className="inline-block flex-none w-1/3">
               <Link href={`/teams/${normalizeName(game.home_team)}`}>
                 <a className="text-blue-500 hover:text-blue-400">{game.home_team}</a>
