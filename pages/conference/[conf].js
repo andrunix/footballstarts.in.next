32c120fd6c918700d886acc9b180b1cbb62c116b
Andrew Pierce <andrunix@gmail.com>
2021-11-08 08:24:20 -0500

    Rendering logos from local using Next/Image.

--- pages/conference/[conf].js
+++ pages/conference/[conf].js
@@ -37,21 +37,21 @@ export default function Conference({teams, conference}) {
           <div className="bg-white px-1 md:px-4 flex-row">
             {teams.map((team) => (
               <div key={team.id} className="teamlogo hover:bg-blue-100 m-2 border border-gray-200 rounded-sm">
-            <img src={team.logos[0]}/>
-            <Link href={`/teams/${team.normalizedName}`}>
-              <a>{team.school}</a>
-            </Link>
-          </div>
-        ))}
+
+                <div className="teamLogoImg">
+                  <Image src={`/images/${team.normalizedName}/${team.id}.png`} width="35px" height="35px"/>
+                </div>
+                
+                <Link href={`/teams/${team.normalizedName}`}>
+                  <a>{team.school}</a>
+                </Link>
+              </div>
+            ))}
             
           </div>
         </div>
       </div>
-
-
-
       
-
     </Layout>
   );
 }
