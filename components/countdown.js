77e8bc30fd3cd2920d8e655a16cbb1544e8a7e15
Andrew Pierce <andrunix@gmail.com>
2021-11-10 12:24:53 -0500

    Adding team colors to countdown

--- components/countdown.js
+++ components/countdown.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
+import { camelCase } from '../lib/teamUtils';
 
 export default function Countdown({teamId, normalized, team, days}) {
   return (
     <div className="text-black text-center bg-opacity-50 bg-blue-500 rounded-t-2xl mx-auto">
       
       <div className="flex justify-center">
+        
         <div className="flex flex-col p-2">
           <Image src={`/images/${normalized}/${teamId}.png`} width="45px" height="45px"/>
         </div>
@@ -14,8 +16,17 @@ export default function Countdown({teamId, normalized, team, days}) {
       </div>
       
       <div className="flex text-5xl md:text-6xl lg:text-7xl xl:text-8xl justify-center mx-auto">
-          <div className="flex flex-col bg-white border-2 border-r-0 border-black rounded-l-xl my-2 p-6">{days}</div>
-          <div className="flex flex-col bg-yellow-600 border-2 border-black rounded-r-xl my-2 p-6">days</div>
+        <div className={["flex flex-col border-2 border-r-0 border-black rounded-l-xl my-2 p-6",
+                         team ? `${camelCase(normalized)}AltColor` : "bg-white"]
+                        .join(' ')}>
+          {days}
+        </div>
+        <div className={["flex flex-col border-2 border-black rounded-r-xl my-2 p-6",
+                         team ? `${camelCase(normalized)}Color` : "bg-yellow-600"]
+                        .join(' ')}>
+          
+          days
+        </div>
       </div>
     </div>
   );
