760687a31fb06849b28cef378cbd03e9fb0d38b0
Andrew Pierce <andrunix@gmail.com>
2021-11-10 12:36:13 -0500

    Adding headers so Chrome does not try to translate.

--- components/layout.js
+++ components/layout.js
@@ -7,10 +7,13 @@ export default function Layout ({children}) {
   return (
     <div className="flex flex-col h-screen justify-between">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8"/>
+        <meta name="google" content="notranslate"/>
+        <meta httpEquiv="Content-Language" content="en"/>
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />    
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>football starts in...</title>
         <meta name="description" content="I want to know when the next college football game is." />
         <link rel="icon" href="/favicon.ico" />
